6bdcb3e6c4f1962146b718be70fb6e232c52eec0
damienst <damien_storimans@hotmail.com>
2015-04-21 11:03:13 +0200

    Update ManyGreetings.py

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -2,5 +2,5 @@
 name = input('Name? ') ##INPUT NAME
 times = int(input('How many greetings? ')) ##INPUT AMOUNT OF GREETINGS
 print('Hello, ' + name + '! ')
-print(times + ' x ' + 'Hello, ' + name + '! ') 
+print(str(times) + ' x ' + 'Hello, ' + name + '! ') 
 
